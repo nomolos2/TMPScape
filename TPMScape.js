ecfca5dda5e0ce1a41e41be25186aadd42ce776f
nomolos2 <solomon@sigfried.org>
2019-11-06 08:56:02 -0500

    Update TPMScape.js

--- TPMScape.js
+++ TPMScape.js
@@ -1,4 +1,22 @@
-let path = math.random
-let path = path * 4
-let path = math.round(path)
-prompt(path)
+function chooseHint(){
+  hint = Math.random()
+  hint = hint * 4
+  hint = Math.round(hint)
+  
+  if (hint == 1) {
+    alert("hi 1");
+  }
+  else if (hint == 2) {
+    alert("hi 2");
+  }
+  else if (hint == 3) {
+    alert("hi 3");
+  }
+   else if (hint == 4) {
+    alert("hi 4");
+  }
+  
+  
+   
+
+  
