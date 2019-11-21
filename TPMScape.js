5bb603006eaac9e36b1eff95018bf56f88874f7a
Solomon Gold <solomon@sigfried.org>
2019-11-21 16:53:29 -0500

    adding for pull

--- TPMScape.js
+++ TPMScape.js
@@ -8,14 +8,13 @@ function playPause() {
   }
   else
   { myVideo.pause();
-   document.getElementById("audioImg").src="images/offVolume.png";
+   document.getElementById("audioImg").src="images/mute2.png";
 }
 }
 
 function establishHints() {
     localStorage.setItem("hints", "Conley|Goehring|Taliff|Porch")
-  }
- 
+}
 
 window.curHint = ""
 function getHint() {
@@ -33,9 +32,6 @@ function getHint() {
   return(hint)
 }
 
-
-
-
 function chooseHint(){
   let hint = getHint()
   alert(`hi ${hint}`)
@@ -52,3 +48,31 @@ function checkHint(name){
 
 }
 
+function timer(){
+var countDownDate = Date.now()+600000;
+
+
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = `${minutes}:${seconds}`;
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+}
